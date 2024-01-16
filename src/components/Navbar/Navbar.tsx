ab2f5bf6e5ee4644b20640b7c1ac3399d17cf91c
zergote <xyges@hotmail.com>
2024-01-16 18:21:42 +0100

    feat: :sparkles: add initial navbar proposal

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,38 @@
 import Link from "next/link";
-
+import Image from "next/image";
+import TwitterIcon from "../../../public/twitter.svg"
+import Sun from "../../../public/sun.svg"
 export default function Navbar() {
   return (
-    <nav className="bg-slate-900">
+    <nav className="bg-blue-600 shadow-md">
       <div className="containner mx-auto flex justify-between items-center py-3">
-        <h3 className="font-bold text-3xl"><Link href="/">Christian Yánez</Link></h3>
-        <ul className="flex gap-x-2 text-lg font-bold">
+        <h3 className="font-bold ml-4 text-sm md:text-xl lg:text-2xl sm:ml-10 md:ml-20 lg:ml-28 xl:ml-32 text-slate-50"><Link href="/">Christian Yánez</Link></h3>
+        <ul className="flex gap-x-2 font-bold mr-4 text-sm md:text-base lg:text-lg sm:mr-10 md:mr-20 lg:mr-28 xl:mr-32">
+          {/*
+            <li>
+              <Image
+                src={Sun}
+                alt={"Light"}
+                className="mt-1"
+                width={20}
+                height={20} />
+            </li>
+          */}
           <li>
-            <Link href="/" className="text-slate-300 hover:text-slate-200">Home</Link>
+            <Link href="/about" className="text-slate-50 mr-1">Sobre mi</Link>
           </li>
           <li>
-            <Link href="/about" className="text-slate-300 hover:text-slate-200">Sobre mi</Link>
-          </li>
-          <li>
-            <Link href="/" className="text-slate-300 hover:text-slate-200">Sigueme</Link>
+            <Link href="https://twitter.com/zergote" className="flex text-slate-50">
+              <Image
+                src={TwitterIcon}
+                alt={"@zergote"}
+                className="mr-1"
+                width={20}
+                height={20} />
+              Sigueme</Link>
           </li>
         </ul>
       </div>
-
     </nav>
-
   )
 }
