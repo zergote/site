2ad3a74bf82e1aa85a834a3d06b83bea26bcb94e
zergote <xyges@hotmail.com>
2024-01-16 19:57:01 +0100

    feat: :sparkles: add initial footer proposal

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="text-slate-50 bg-blue-600 drop-shadow-lg mt-auto h-8">
+      <div className="flex justify-between mt-1.5">
+        <div className="text-sm ml-4 sm:ml-10 md:ml-20 lg:ml-28 xl:ml-32">
+          Christian Yánez <a href="https://x.com/zergote">(@zergote)</a>
+        </div>
+        <div className="text-sm mr-4 sm:mr-10 md:mr-20 lg:mr-28 xl:mr-32"><a href="https://github.com/zergote/site">Source</a></div>
+      </div>
+    </footer>
+  )
+}
