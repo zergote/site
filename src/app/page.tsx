7dcc752a937e8c524c6176ce7bc80295f64f2c9d
zergote <xyges@hotmail.com>
2024-01-17 09:01:39 +0100

    feat: :sparkles: complete structure of PersonalOverview component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import PersonOverview from "@/components/PersonOverview/PersonOverview";
 
 export default function Home() {
+  //max-w-screen-2xl 
   return (
-    <div className="container max-w-screen-lg ml-4 mr-4 sm:ml-10 md:ml-20 lg:ml-28 xl:ml-32 sm:mr-10 md:mr-20 lg:mr-28 xl:mr-32">
-      <h1>Christian Yánez Sitio Web</h1>
-      <PersonOverview />
+    <div className="min-w-96">
+      <div className="containner ml-4 mr-4 py-3 max-w-screen-sm md:max-w-screen-md lg:max-w-screen-md sm:mx-auto">
+        <PersonOverview />
+      </div>
     </div>
   )
 }
--- src/components/PersonOverview/PersonOverview.tsx
+++ src/components/PersonOverview/PersonOverview.tsx
@@ -12,15 +12,16 @@ export default function PersonOverview() {
   }
 
   return (
-    <div className="containner bg-slate-600 max-w-96">
-      <div className="flex items-center text-center flex-col rounded-lg p-3">
-        <p>Mi nombre es Christian Yánez, crecí en Irapa/Sucre, una hermosa ciudad del oriente de Venezuela. Trabajo profesionalmente en el desarrollo de software para computadoras en diferentes roles: he sido fronted, backendm devops, asi como ingeniero de software, gerente de producto. Cuando la vida y el trabajo lo permiten, disfruto publicando y compartiendo todo lo que descubro o invento en este mismo sitio web.
+    <div className="containner bg-slate-600">
+      <div className="flex flex-col rounded-lg p-3">
+        <p>Mi nombre es Christian Yánez, crecí en Irapa/Sucre, una hermosa ciudad del oriente de Venezuela. Trabajo profesionalmente en el desarrollo de software para computadoras en diferentes roles: he sido fronted, backend, devops, asi como ingeniero de software y gerente de producto. Cuando la vida y el trabajo lo permiten, disfruto publicando y compartiendo todo lo que descubro o invento en este mismo sitio web.
         </p>
-        <a href="tel:+34679787002">+34 679 78 70 02</a>
-        <div>
-          <a href="mailto:christian@yanezc.com">christian@yanezc.com</a>
-          <button onClick={handleCopyMail}>COPIAR</button>
-          {visible && <span className="text-red-700 absolute inline-block border-dotted border-red-600 z-10 bg-slate-50 ">email copiado</span>}
+        <hr />
+        <div className="">
+          <span>
+            Si estás interesado en conocerme, no lo dudes y ponte en contacto conmigo en <a href="mailto:christian@yanezc.com" className="">christian@yanezc.com</a><button onClick={handleCopyMail} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1 rounded ml-2">COPIAR</button>
+            {visible && <span className="text-red-700 absolute inline-block border-dotted border-red-600 z-10 bg-slate-50 ">email copiado</span>} o <a href="tel:+34679787002">+34 679 78 70 02</a>
+          </span>
         </div>
       </div>
     </div>
