6ff31f3d797ba2f9e33f26ab1295f1134ef345c3
zergote <xyges@hotmail.com>
2024-01-18 19:05:34 +0100

    fix: :lipstick: set styles in BottomNav component

--- src/components/BottomNav/BottomNav.tsx
+++ src/components/BottomNav/BottomNav.tsx
@@ -1,6 +1,6 @@
 export default function BottomNav() {
   return (
-    <div className="bg-white text-black font-bold text-sm sticky bottom-0  sm:relative mt-5 rounded-t-lg font-mono pl-3 pr-3 border-t border-gray-600">
+    <div className="bg-white text-black font-bold text-sm sticky bottom-0  sm:relative mt-5 rounded-t-lg font-mono pl-3 pr-3 sm:pl-16 sm:pr-16 border-t border-gray-600">
       <ul className="flex justify-between items-center text-center">
         <li><a href="https://github.com/zergote">GitHub</a></li>
         <li>|</li>
