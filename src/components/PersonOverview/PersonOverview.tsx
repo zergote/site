8f2b7e0b8f1171eeae21c0d98d522d3ad4f59dcf
zergote <xyges@hotmail.com>
2024-01-31 12:34:13 +0100

    style: :lipstick: add efects hover in svg icon

--- src/components/PersonOverview/PersonOverview.tsx
+++ src/components/PersonOverview/PersonOverview.tsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 
 export default function PersonOverview() {
   const [visible, setVisible] = useState(false)
+  const [hg, setHg] = useState(false)
 
   const handleCopyMail = (event: React.MouseEvent<HTMLElement>): void => {
     navigator.clipboard.writeText("christian@yanezc.com")
@@ -18,8 +19,8 @@ export default function PersonOverview() {
         <p className="print:hidden">Cuando la vida y el trabajo lo permitan, disfrutaré publicando y compartiendo todo lo que descubro o invento en este sitio web.</p>
         <div>
           <span>
-            Si estás interesado en conocerme, no lo dudes y ponte en contacto conmigo en <a href="mailto:christian@yanezc.com" className="bg-[#f0f1f2] border border-blue-700 hover:border-indigo-700 p-1 font-aino underline print:border-none print:bg-transparent">christian@yanezc.com</a> {visible && <span className="text-white bg-[#0000f0] border border-solid font-ainoHeadline p-1"> email copiado!</span>} {!visible && <button onClick={handleCopyMail}>
-              <svg className="pt-1 w-4 h-4 text-gray-800 dark:text-white print:hidden" aria-hidden="true" fill="none" viewBox="0 0 18 20">
+            Si estás interesado en conocerme, no lo dudes y ponte en contacto conmigo en <a href="mailto:christian@yanezc.com" className={`rounded border border-blue-700 p-1 font-aino underline ${hg ? "text-white bg-[#0000f0]" : "bg-[#f0f1f2]"} print:border-none print:bg-transparent `}>christian@yanezc.com</a> {visible && <span className="text-white bg-[#0000f0] border border-solid font-ainoHeadline p-1"> email copiado!</span>} {!visible && <button onClick={handleCopyMail}>
+              <svg onMouseEnter={() => { setHg(true) }} onMouseLeave={() => { setHg(false) }} className="pt-1 w-4 h-4 text-[#575a5d] hover:text-[#323334] print:hidden" aria-hidden="true" fill="none" viewBox="0 0 18 20">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m7.708 2.292.706-.706A2 2 0 0 1 9.828 1h6.239A.97.97 0 0 1 17 2v12a.97.97 0 0 1-.933 1H15M6 5v4a1 1 0 0 1-1 1H1m11-4v12a.97.97 0 0 1-.933 1H1.933A.97.97 0 0 1 1 18V9.828a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 1 5.828 5h5.239A.97.97 0 0 1 12 6Z" />
               </svg>
             </button>}
