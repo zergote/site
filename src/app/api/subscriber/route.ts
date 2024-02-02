ab74f07b5b938ab9cce3c5c47eecd61d95330ab9
zergote <xyges@hotmail.com>
2024-02-02 13:23:30 +0100

    chore: enable send email feature

--- src/app/api/subscriber/route.ts
+++ src/app/api/subscriber/route.ts
@@ -15,22 +15,22 @@ export async function POST(request: any) {
   console.log(newSubscriber)
 
   // Require:
-  /*
-    // Send an email:
-    var client = new postmark.ServerClient(String(process.env.API_POSTMARK));
-    try {
-      client.sendEmail({
-        "From": "christian@yanezc.com",
-        "To": email,
-        "Subject": "Bienvenido la lista de correos",
-        "HtmlBody": "Hola bienvenido, en estos mails recibiras consejos y mensajes brujula sobre el mundo del desarrollo. Puedes darte de baja cuando quieras escribiendo a christian@yanezc.com. Espero que disfrutes recibiendo el contenido.",
-        "TextBody": "",
-        "MessageStream": "outbound"
-      });
-    } catch (error) {
-      console.log(error)
-    }
-  */
+
+  // Send an email:
+  var client = new postmark.ServerClient(String(process.env.API_POSTMARK));
+  try {
+    client.sendEmail({
+      "From": "christian@yanezc.com",
+      "To": email,
+      "Subject": "Bienvenido la lista de correos",
+      "HtmlBody": "Hola bienvenido, en estos mails recibiras consejos y mensajes brujula sobre el mundo del desarrollo. Puedes darte de baja cuando quieras escribiendo a christian@yanezc.com. Espero que disfrutes recibiendo el contenido.",
+      "TextBody": "",
+      "MessageStream": "outbound"
+    });
+  } catch (error) {
+    console.log(error)
+  }
+
   return NextResponse.json(newSubscriber)
 }
 
